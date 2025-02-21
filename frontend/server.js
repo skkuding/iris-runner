2786a2bd97f8c5ede62f1c516d5085dcae122a64
donghun1214 <acekdhh@g.skku.edu>
2025-02-21 21:29:08 +0900

    feat(iris): implement prototype of iris-runner

---
+++ frontend/server.js
@@ -0,0 +1,32 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const hostname = '0.0.0.0';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    const filePath = path.join(__dirname, 'index.html');
+    
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Internal Server Error');
+      } else {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.end(data);
+      }
+    });
+  } else {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Not Found');
+  }
+});
+
+server.listen(port, hostname, () => {
+  console.log(`서버가 http://${hostname}:${port}/ 에서 실행 중입니다.`);
+});
